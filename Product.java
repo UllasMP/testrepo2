ebb89c5b574b15c0c434de30363da840159b6e45
Dev1 <ullasmpu@gmail.com>
2026-03-28 11:01:07 +0530

    Created java file from dev1

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int pr=a*b;
+        System.out.println("The product of a and b is: " + pr);
+    }
+    
+}
