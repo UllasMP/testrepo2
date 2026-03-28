6c40ea9408afed800f63eee66fc0efe4e1c08249
Dev1 <ullasmpu@gmail.com>
2026-03-28 11:12:21 +0530

    modified sub.java from dev1 branch1

--- sub.java
+++ sub.java
@@ -4,5 +4,12 @@ public class sub{
         int b = 10;
         int sub = a - b;
         System.out.println("The difference of a and b is: " + sub);
+        if(sub < 0){
+            System.out.println("The result is negative.");
+        } else if (sub > 0){
+            System.out.println("The result is positive.");
+        } else {
+            System.out.println("The result is zero.");
+        }
     }
 }   
