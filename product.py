3ddcce7179317dd7f2e3c51e90c269515f25e51e
dev2 <ullasmpu@gmail.com>
2026-03-28 11:07:04 +0530

    added python files from dev2

---
+++ product.py
@@ -0,0 +1,7 @@
+a=[1, 2, 3, 4, 5]
+def sum(a):
+    sum=0
+    for i in a:
+        sum*=i
+    return sum
+print(sum(a))
---
+++ sub.py
@@ -0,0 +1,5 @@
+a=1099
+b=2011
+def sub(a, b):
+    return a-b
+print(sub(a, b))
